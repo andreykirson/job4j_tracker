4eb350bf8fdd3a5033629499732cb006d992eaad
andreykirson <a.s.kirson@gmail.com>
2020-04-21 20:02:17 +1000

    1. Конструктор по умолчанию.[#268549]

---
+++ src/main/java/ru/job4j/tracker/Predator.java
@@ -0,0 +1,11 @@
+package ru.job4j.tracker;
+
+
+public class Predator extends Animal {
+    public Predator(String name) {
+        super(name);
+        System.out.println("Predator");
+    }
+}
+
+
