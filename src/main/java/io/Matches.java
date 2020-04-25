f1f899bcdfa82af1fc37d2860dac1ce3887dec91
andreykirson <a.s.kirson@gmail.com>
2020-04-26 09:40:33 +1000

    2. Scanner и чтение числа из консоли.[#268500]

---
+++ src/main/java/io/Matches.java
@@ -0,0 +1,30 @@
+package io;
+
+import java.util.Scanner;
+
+public class Matches {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int matches = 11;
+        while(matches!= 0) {
+            System.out.println("Игрок 1 возьмите  от 1 до 3 сипчек");
+            int playerFirst = Integer.valueOf(input.nextLine());
+            matches = matches - playerFirst;
+            System.out.println("На столе осталось " + matches + " сипчек");
+            if(matches == 0) {
+                System.out.println("Игрок 1 выйграл");
+                break;
+            }
+
+            System.out.println("Игрок 2 возьмите  от 1 до 3 сипчек");
+            int playerSecond = Integer.valueOf(input.nextLine());
+            matches = matches - playerSecond;
+            System.out.println("На столе осталось " + matches  + " сипчек");
+            if(matches == 0) {
+                System.out.println("Игрок 2 выйграл");
+                break;
+            }
+
+        }
+    }
+}
