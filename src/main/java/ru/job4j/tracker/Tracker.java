45e084c289a7e02449ef22106a94da8048f5806c
andreykirson <a.s.kirson@gmail.com>
2020-04-25 13:49:34 +1000

    5. Реализовать класс Tracker[#268563]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -1,23 +1,36 @@
 package ru.job4j.tracker;
 
+import java.util.Arrays;
+import java.util.Random;
+
+/**
+ * @version $Id$
+ * @since 0.1
+ */
 public class Tracker {
+    /**
+     * Массив для хранение заявок.
+     */
     private final Item[] items = new Item[100];
     private int ids = 1;
-    private int size = 0;
+    /**
+     * Указатель ячейки для новой заявки.
+     */
+    private int position = 0;
 
+    /**
+     * Метод реализующий добавление заявки в хранилище
+     * @param item новая заявка
+     */
     public Item add(Item item) {
         item.setId(generateId());
-        items[size++] = item;
+        this.items[position++] = item;
         return item;
-    }
-
-    private String generateId() {
-        return String.valueOf(ids++);
     }
 
     public Item findById(String id) {
         Item rsl = null;
-        for (int index = 0; index < size; index++) {
+        for (int index = 0; index < items.length; index++) {
             Item item = items[index];
             if (item.getId().equals(id)) {
                 rsl = item;
@@ -25,5 +38,41 @@ public class Tracker {
             }
         }
         return rsl;
+    }
+
+    /**
+     * Метод генерирует уникальный ключ для заявки.
+     * Так как у заявки нет уникальности полей, имени и описание. Для идентификации нам нужен уникальный ключ.
+     * @return Уникальный ключ.
+     */
+    private String generateId() {
+        Random rm = new Random();
+        return String.valueOf(rm.nextLong() + System.currentTimeMillis());
+    }
+
+    public Item findByName(String key){
+        Item rsl = null;
+        for (int index = 0; index < items.length; index++) {
+            Item item = items[index];
+            if (item.getName().equals(key)) {
+                rsl = item;
+                break;
+            }
+        }
+        return rsl;
+    }
+
+    public Item[] findAll(){
+        Item[] itemsWithoutNull = new Item[items.length];
+        int size = 0;
+        for (int index = 0; index < items.length; index++) {
+            Item item = items[index];
+            if (item != null) {
+                itemsWithoutNull[size] = item;
+                size++;
+            }
+        }
+        itemsWithoutNull = Arrays.copyOf(itemsWithoutNull, size);
+        return itemsWithoutNull;
     }
 }
--- src/test/java/ru/job4j/tracker/TrackerTest.java
+++ src/test/java/ru/job4j/tracker/TrackerTest.java
@@ -13,4 +13,30 @@ public class TrackerTest {
         Item result = tracker.findById(item.getId());
         assertThat(result.getName(), is(item.getName()));
     }
+
+    @Test
+    public void findByName() {
+        Tracker tracker = new Tracker();
+        Item item_1 = new Item("test1");
+        Item item_2 = new Item("test2");
+        tracker.add(item_1);
+        tracker.add(item_2);
+        Item result = tracker.findByName(item_1.getName());
+        assertThat(result.getName(), is(item_1.getName()));
+    }
+
+    @Test
+    public void findAll() {
+        Tracker tracker = new Tracker();
+        Item[] result = new Item[100];
+        Item item_1 = new Item("test1");
+        Item item_2 = new Item("test2");
+        tracker.add(item_1);
+        tracker.add(item_2);
+        result = tracker.findAll();
+        assertThat(result, is(tracker.findAll()));
+    }
+
+
+
 }
