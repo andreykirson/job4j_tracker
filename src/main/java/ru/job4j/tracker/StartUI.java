de0b7b2761081002ca04baa35a20e82959d825d5
andreykirson <a.s.kirson@gmail.com>
2020-04-26 16:33:56 +1000

    4.2. Статические методы.[#268506]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -12,6 +12,38 @@ package ru.job4j.tracker;
  */
 
 public class StartUI {
+
+    public static void createItem(Input input, Tracker tracker) {
+        System.out.println("=== Create a new Item ====");
+        String name = input.askStr("Enter name: ");
+        Item item = new Item(name);
+        tracker.add(item);
+    }
+
+    public static void replaceItem(Input input, Tracker tracker) {
+        System.out.println("==== Edit item ====");
+        String id = input.askStr("Input id of item:");
+        String name = input.askStr("Input the of item to edit name item:");
+        Item repItem = new Item(name);
+        if (tracker.replace(id, repItem)) {
+            System.out.println("==== The item is edited ====");
+        } else {
+            System.out.println("==== The item by id" + id + "does not exist ====");
+        }
+    }
+
+    public static void deteleItem(Input input, Tracker tracker) {
+        System.out.println("==== Delete item ====");
+        String id = input.askStr("Input id of item:");
+        if(tracker.delete(id)) {
+            System.out.println("==== The item by id " + id + "is deleted ===");
+        } else {
+            System.out.println("==== The item by id " + id + "does not exist ===");
+        }
+    }
+    //
+
+
     public void init(Input input, Tracker tracker) {
         boolean run = true;
         while (run) {
