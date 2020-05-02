39101bda770ed5ac529f1056435642cdd546bf63
andreykirson <a.s.kirson@gmail.com>
2020-05-02 11:35:41 +1000

    2.1. Реализация класса StartUI[#268510]

--- src/main/java/ru/job4j/tracker/FindByIdAction.java
+++ src/main/java/ru/job4j/tracker/FindByIdAction.java
@@ -3,14 +3,18 @@ package ru.job4j.tracker;
 public class FindByIdAction implements UserAction {
     @Override
     public String name() {
-        return "==== Find item by Id ====";
+        return "==== Searching item by Id ====";
     }
 
     @Override
     public boolean execute(Input input, Tracker tracker) {
         String id = input.askStr("Input id of item:");
         Item findItem = tracker.findById(id);
-        System.out.println(findItem.getId() + " " + findItem.getName());
+        if ( findItem != null) {
+            System.out.println(findItem.getId() + " " + findItem.getName());
+        } else {
+            System.out.println("Item with id " + id + " didn't find");
+        }
         return false;
     }
 
--- src/main/java/ru/job4j/tracker/FindbyNameAction.java
+++ src/main/java/ru/job4j/tracker/FindbyNameAction.java
@@ -1,15 +1,23 @@
 package ru.job4j.tracker;
 
+import java.awt.desktop.SystemEventListener;
+import java.util.Arrays;
+
 public class FindbyNameAction implements UserAction {
     @Override
     public String name() {
-        return "==== Find items by name ====";
+        return "==== Searching items by name ====";
     }
 
     @Override
     public boolean execute(Input input, Tracker tracker) {
         String name = input.askStr("Input the name of items");
-        StartUI.printStr(tracker.findByName(name));
+        Item[] item = tracker.findByName(name);
+        if (item.length == 0){
+            System.out.println("The item by name " + name + " didn't find");
+        } else{
+            System.out.println(Arrays.toString(item));
+        }
         return false;
     }
 
