de99b6cd2ad5fed7f95964e6c5791b78eee3a3d0
andreykirson <a.s.kirson@gmail.com>
2020-05-14 21:37:29 +1000

    2. Идентичные тексты.[#268492]

---
+++ src/main/java/collection/UniqueText.java
@@ -0,0 +1,22 @@
+package collection;
+
+import java.util.HashSet;
+
+public class UniqueText {
+    public static boolean isEquals(String originText, String duplicateText) {
+        boolean rsl = true;
+        String[] origin = originText.split(" ");
+        String[] text = duplicateText.split(" ");
+        HashSet<String> check = new HashSet<>();
+
+        for (String s:origin) {
+            check.add(s);
+        }
+        for (String t:text) {
+            if(!check.contains(t)){
+             rsl = false;
+            }
+        }
+        return rsl;
+    }
+}
