4f981e475bf63849df5d056c7d7450c6d22d4fad
andreykirson <a.s.kirson@gmail.com>
2020-05-14 21:51:10 +1000

    3. Модели данных и HashSet.[#268493]

---
+++ src/main/java/collection/NotifyAccount.java
@@ -0,0 +1,15 @@
+package collection;
+
+import java.util.HashSet;
+import java.util.List;
+
+public class NotifyAccount {
+    public static HashSet<Account> sent(List<Account> accounts) {
+        HashSet<Account> rsl = new HashSet<>();
+        // for-each accounts to HashSet;
+        for (Account account:accounts) {
+            rsl.add(account);
+        }
+        return rsl;
+    }
+}
