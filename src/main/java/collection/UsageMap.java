6af56a74a80e9309b023c47271571e02e043eb0f
andreykirson <a.s.kirson@gmail.com>
2020-05-15 20:54:01 +1000

    0. Map, HashMap[#268486]

---
+++ src/main/java/collection/UsageMap.java
@@ -0,0 +1,14 @@
+package collection;
+
+import java.util.HashMap;
+
+public class UsageMap {
+    public static void main(String[] args) {
+        HashMap<String, String> map = new HashMap<>();
+        map.put("a.s.kirson@gmail.com", "Andrey Kirson" );
+        for(String key : map.keySet()) {
+            String value = map.get(key);
+            System.out.println(key + " = " + value);
+        }
+    }
+}
