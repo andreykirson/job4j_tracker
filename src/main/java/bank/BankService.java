9a2f354803020d62fd5512b6711113c5f7689da6
andreykirson <a.s.kirson@gmail.com>
2020-05-20 07:22:34 +1000

    3. Банковские переводы.[#268488]

--- src/main/java/bank/BankService.java
+++ src/main/java/bank/BankService.java
@@ -19,7 +19,7 @@ public class BankService {
         if (accounts.size() == 0) {
             accounts.add(new Account(account.getRequisite(), account.getBalance()));
         } else {
-                if (accounts.contains(account.getRequisite()) != true) {
+                if (!accounts.contains(account){
                     accounts.add(account);
                 }
         }
