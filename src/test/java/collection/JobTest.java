8c60ffaedb2ac062aad60f0b8332dfc60d02fbca
andreykirson <a.s.kirson@gmail.com>
2020-05-20 07:43:17 +1000

    2. Комбинированный компаратор.[#268475]

--- src/test/java/collection/JobTest.java
+++ src/test/java/collection/JobTest.java
@@ -7,6 +7,7 @@ import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 
+import static org.hamcrest.Matchers.greaterThan;
 import static org.hamcrest.Matchers.lessThan;
 import static org.junit.Assert.assertThat;
 
@@ -16,7 +17,7 @@ public class JobTest {
         Comparator<Job> cmpNamePriority = new JobSortByName().thenComparing(new JobSortByPriority());
         int rsl = cmpNamePriority.compare(
                 new Job("A", 0),
-                new Job("B", 1)
+                new Job("A", 1)
         );
         assertThat(rsl, lessThan(0));
         System.out.println(rsl);
@@ -27,7 +28,7 @@ public class JobTest {
         Comparator<Job> cmpNamePriority = new JobSortByPriority().thenComparing(new JobSortByName());
         int rsl = cmpNamePriority.compare(
                 new Job("A", 0),
-                new Job("B", 1)
+                new Job("B", 0)
         );
         assertThat(rsl, lessThan(0));
         System.out.println(rsl);
@@ -38,19 +39,32 @@ public class JobTest {
         Comparator<Job> cmpNamePriority = new JobSortByName().thenComparing(new JobReverseSortByPriority());
         int rsl = cmpNamePriority.compare(
                 new Job("A", 0),
-                new Job("B", 1)
+                new Job("A", 1)
         );
-        assertThat(rsl, lessThan(0));
+        assertThat(rsl, greaterThan(0));
         System.out.println(rsl);
     }
 
     @Test
     public void whenCompatorByProrityAndDescName() {
+        Comparator<Job> cmpNamePriority = new JobSortByPriority().thenComparing(new JobReversSortByName());
+        int rsl = cmpNamePriority.compare(
+                new Job("A", 0),
+                new Job("B", 0)
+        );
+        assertThat(rsl, greaterThan(0));
+        System.out.println(rsl);
+    }
+
+
+
+    @Test
+    public void PrintResult() {
         List<Job> jobs = Arrays.asList(
                 new Job("A", 2),
                 new Job("A", 4),
-                new Job("C", 5),
-                new Job("D", 2)
+                new Job("A", 5),
+                new Job("A", 2)
         );
         Collections.sort(jobs, new JobSortByPriority().thenComparing(new JobReversSortByName()));
         System.out.println(jobs.toString());
