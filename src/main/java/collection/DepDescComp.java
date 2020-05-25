ce8fe9b0a3df60d9373bc637570974bcb2e3d5d6
andreykirson <a.s.kirson@gmail.com>
2020-05-25 21:13:44 +1000

    Отсортировать департаменты[34352#268469]

--- src/main/java/collection/DepDescComp.java
+++ src/main/java/collection/DepDescComp.java
@@ -1,9 +1,6 @@
 package collection;
 
-import java.util.ArrayList;
 import java.util.Comparator;
-import java.util.List;
-
 
 /**
  * Отсортировать департаменты [#34352].
@@ -13,49 +10,13 @@ import java.util.List;
  */
 
 public class DepDescComp implements Comparator<String> {
-
-    /**
-     * Comaparator with the special sort order.
-     * @param o1
-     * @param o2
-     * @return - result of comparing element of list.
-     */
-
     @Override
     public int compare(String o1, String o2) {
-        List<String> l1 = strConvertList(o1);
-        List<String> l2 = strConvertList(o2);
-        int c = 0;
-        for (int i = 0; i < Math.min(l1.size(), l2.size()); i++) {
-            if (i == 0) { // to reverse order sort by first element
-                c = l2.get(i).compareTo(l1.get(i));
-                if (c != 0) {
-                    return c;
-                }
-            } else {
-                c = l1.get(i).compareTo(l2.get(i));
-                if (c != 0) {
-                    return c;
-                }
-            }
-        }
-        return Integer.compare(l1.size(), l2.size());
+      String[] o1Splited = o1.split("/");
+      String[] o2Splited = o2.split("/");
+      int firstStr = o2Splited[0].compareTo(o1Splited[0]);
+      return firstStr == 0 ?  o1.compareTo(o2) : o2.compareTo(o1);
     }
-
-    /**
-     * Method to convert department structure to list to use this structure of data to compare them by special order.
-     * @param str - list of names departments.
-     * @return - list of names splited to elements.
-     */
-
-    public List<String> strConvertList(String str) {
-        List<String> list = new ArrayList<String>();
-        for (String el : str.split("/")) {
-            list.add(el);
-        }
-        return list;
-    }
-
 }
 
 
