49777e3a1dbd0ab2ec495ad35487e91dcdabb55d
andreykirson <a.s.kirson@gmail.com>
2020-05-30 19:12:26 +1000

    5. Преобразование матрицы чисел в список чисел.[110228#268349]

--- src/main/java/stream/MatrixToList.java
+++ src/main/java/stream/MatrixToList.java
@@ -3,6 +3,11 @@ package stream;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+/**
+ * Stream.of is generic,  Arrays.stream works with the primitive data type
+ */
 
 public class MatrixToList {
 
@@ -19,8 +24,23 @@ public class MatrixToList {
                 .collect(Collectors.toList());
 
         System.out.println(matrix);
-
         System.out.println(flatMatrix);
+
+        Integer[][] data = new Integer[][]{{1, 2}, {3, 4}, {5, 6}};
+        Stream<Integer[]> temp = Arrays.stream(data);
+        List<Integer> integerStream = temp.flatMap(x -> Arrays.stream(x))
+                .collect(Collectors.toList());
+
+        System.out.println(data);
+        System.out.println(integerStream);
+
+        Stream<Integer[]> t = Stream.of(data);
+
+        List<Integer> intStreamNew = t.flatMap(Arrays::stream)
+                .collect(Collectors.toList());
+
+
+        System.out.println(intStreamNew);
     }
 
 }
