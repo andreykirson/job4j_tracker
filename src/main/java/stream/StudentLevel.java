1f1a220e6a0702e852f3b19ab23dabe0d161d9dd
andreykirson <a.s.kirson@gmail.com>
2020-05-31 20:12:42 +1000

    0. Stream API улучшения[58167#268353]

---
+++ src/main/java/stream/StudentLevel.java
@@ -0,0 +1,17 @@
+package stream;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class StudentLevel {
+    public static List<Student> levelOf(List<Student> students, int bound) {
+        return (students.stream()
+                .flatMap(Stream::ofNullable)
+                .filter(st -> st.getScore() > bound)
+                .sorted(Comparator.comparingInt(Student::getScore))
+                .takeWhile(st -> st.getScore() >  bound)
+                .collect(Collectors.toList()));
+    }
+}
