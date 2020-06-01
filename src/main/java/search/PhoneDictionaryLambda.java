92e8437f65824722b0edebac45fb8c56ccd9f8fc
andreykirson <a.s.kirson@gmail.com>
2020-06-01 21:58:02 +1000

    2. Local-Variable Type Inference[70626#268355]

--- src/main/java/search/PhoneDictionaryLambda.java
+++ src/main/java/search/PhoneDictionaryLambda.java
@@ -18,7 +18,7 @@ public class PhoneDictionaryLambda {
         Predicate<Person> combine = containName.or(containSurName.or(containAddress.or(containPhone)));
 
         ArrayList<Person> result = new ArrayList<>();
-        for (Person person : persons) {
+        for (var person : persons) {
             if (combine.test(person)) {
                 result.add(person);
             }
