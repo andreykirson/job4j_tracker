c4834f01729916d938ee3927b5f6b1fc54274e5c
andreykirson <a.s.kirson@gmail.com>
2020-08-02 13:47:15 +1000

    4.4. Liquibase. Интеграционные тесты. Proxy. [#302180]

---
+++ src/main/java/ru/job4j/tracker/TrackerSQLTest.java
@@ -0,0 +1,33 @@
+package ru.job4j.tracker;
+
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class TrackerSQLTest {
+    public Connection init() {
+        try (InputStream in = TrackerSQL.class.getClassLoader().getResourceAsStream("app.properties")) {
+            Properties config = new Properties();
+            config.load(in);
+            Class.forName(config.getProperty("driver-class-name"));
+            return DriverManager.getConnection(
+                    config.getProperty("url"),
+                    config.getProperty("username"),
+                    config.getProperty("password")
+
+            );
+        } catch (Exception e) {
+            throw new IllegalStateException(e);
+        }
+    }
+
+    @Test
+    public void createItem() throws SQLException {
+        try (TrackerSQL tracker = new TrackerSQL(ConnectionRollback.create(this.init()))) {
+            tracker.add(new Item("name", "desc"));
+            assertThat(tracker.findByName("desc").size(), is(1));
+        }
+    }
+}
