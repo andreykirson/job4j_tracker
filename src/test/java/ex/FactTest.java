668f7d09347c295c06e22850eeac57eef0b2f225
andreykirson <a.s.kirson@gmail.com>
2020-08-17 22:17:55 +1000

    4.5. Travis CI и PostgreSQL [#302181]

--- src/test/java/ex/FactTest.java
+++ src/test/java/ex/FactTest.java
@@ -5,7 +5,7 @@ import org.junit.Test;
 public class FactTest {
 
     @Test(expected = IllegalArgumentException.class)
-    public void WhenNLessThenZero() {
+    public void whenNLessThenZero() {
         Fact fact = new Fact();
         int n = -1;
         int rsl = fact.calc(n);
